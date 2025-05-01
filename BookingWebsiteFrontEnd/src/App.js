504cf546642c6a0419e2d2de35f1630f454f48fb
tiendat2001 <damtiendat2001@gmail.com>
2025-05-01 17:44:41 +0700

    update route api

--- BookingWebsiteFrontEnd/src/App.js
+++ BookingWebsiteFrontEnd/src/App.js
@@ -52,7 +52,7 @@ function App() {
     useEffect(() => {
       const checkAccessToken = async () => {
         try {
-          const response = await fetch('api/auth/checkHasAccessToken');
+          const response = await axios.get("/api/auth/checkHasAccessToken");
           if (response.status === 200) {
             setIsLoggedIn(true);
           }
@@ -90,7 +90,7 @@ function App() {
     useEffect(() => {
       const checkAccessToken = async () => {
         try {
-          const response = await fetch('api/auth/checkHasAccessTokenAdministrator');
+          const response = await axios.get("/api/auth/checkHasAccessTokenAdministrator");
           if (response.status === 200) {
             setIsLoggedIn(true);
           }
