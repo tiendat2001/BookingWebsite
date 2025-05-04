b82f42e03a72ec658d0c4094d312251589d2e4e4
tiendat2001 <damtiendat2001@gmail.com>
2025-05-04 11:07:10 +0700

    update socket url

--- BookingWebsiteFrontEnd/src/components/adminComponents/navbarAdmin/NavbarAdmin.jsx
+++ BookingWebsiteFrontEnd/src/components/adminComponents/navbarAdmin/NavbarAdmin.jsx
@@ -12,7 +12,7 @@ const NavbarAdmin = () => {
   const socketRef = useRef(null);
 
   useEffect(() => {
-    socketRef.current = io("https://bookingwebsite-vhgs.onrender.com");
+    socketRef.current = io(process.env.SOCKET_URL);
 
     socketRef.current.on("connect", () => {
       axios.post("/api/notification/connect-socket", {
